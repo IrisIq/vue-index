96338399de357dfd3a737ee009df08d834a08b03
IrisIq <2637852337@qq.com>
2024-05-14 15:12:25 +0800

    feat:去除无用页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,11 @@
+/*
+ * @Author: IrisIq
+ * @Date: 2022-07-02 20:59:08
+ * @LastEditors: IrisIq
+ * @LastEditTime: 2024-05-14 15:12:05
+ * @Description: content
+ */
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
-import test from '../views/test.vue'
 import calculatorView from '../views/calculatorView.vue'
 
 const routes = [
@@ -7,11 +13,6 @@ const routes = [
     path: '/',
     name: 'calculatorView',
     component: calculatorView
-  },
-  {
-    path: '/test',
-    name: 'test',
-    component: test
   }
 ]
 
